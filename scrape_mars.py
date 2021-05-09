6f5a17af68815bf743a6c2b90bec26c0bb0b3df9
Thomas Stenner <thomasstenner@thomass-mbp.attlocal.net>
2021-05-09 18:44:00 -0500

    new

--- scrape_mars.py
+++ scrape_mars.py
@@ -21,3 +21,12 @@ def scrape():
     # Retrieve the latest news title and paragraph
     news_title = news_soup.find_all('div', class_='content_title')[0].text
     news_p = news_soup.find_all('div', class_='article_teaser_body')[0].text
+
+    # Mars Image to be scraped
+    jpl_nasa_url = 'https://www.jpl.nasa.gov'
+    images_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(images_url)
+    html = browser.html
+    images_soup = BeautifulSoup(html, 'html.parser')
+    relative_image_path = images_soup.find_all('img')[3]["src"]
+    featured_image_url = jpl_nasa_url + relative_image_path
